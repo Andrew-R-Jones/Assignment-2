c9394733858326a89bff149e276e9766a04216da
Andrew Jones <andrew_52490@yahoo.com>
2019-02-15 18:28:54 -0700

    Class and methods documented using javadoc standards

--- Assignment 2/src/cse360assign2/Calculator.java
+++ Assignment 2/src/cse360assign2/Calculator.java
@@ -2,6 +2,12 @@
  * @author Andrew Jones
  * @classID 427
  * @assignmentNumber 2
+ * 
+ * This class acts as a calculator. It is able to do calculations such as
+ * add, subtract, multiply and divide. All calculations are computed on the
+ * running total which it initialized to zero.
+ * 
+ * 
  */
 
 package cse360assign2;
@@ -9,33 +15,70 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-	
-	public Calculator () {
-		total = 0;  // not needed - included for clarity
+
+	public Calculator() {
+		total = 0; // not needed - included for clarity
 	}
-	
-	public int getTotal () {
+
+	/**
+	 * This getter method returns the current total
+	 * 
+	 * @return running total of all calculations
+	 */
+	public int getTotal() {
 		return total;
 	}
-	
-	public void add (int value) {
+
+	/**
+	 * This method adds an integer value to the running total
+	 * 
+	 * @param value value which is added to the total
+	 */
+	public void add(int value) {
 		total += value;
 	}
-	
-	public void subtract (int value) {
+
+	/**
+	 * This method subtracts an integer value from the running total
+	 * 
+	 * @param value value to be subtracted from the total
+	 */
+	public void subtract(int value) {
 		total -= value;
 	}
-	
-	public void multiply (int value) {
+
+	/**
+	 *
+	 * This method multiplies an integer value by the running total
+	 * 
+	 * @param value value which the total is multiplied by
+	 */
+	public void multiply(int value) {
 		total *= value;
 	}
-	
-	public void divide (int value) {
-		if (value == 0) total = 0;
-		else total /= value;
+
+	/**
+	 * 
+	 * This method divides the running total by an integer value If the value passed
+	 * in is zero, the total is set to zero, else normal integer division is applied
+	 * 
+	 * @param value value which the total is divided by
+	 */
+	public void divide(int value) {
+		if (value == 0)
+			total = 0;
+		else
+			total /= value;
 	}
-	
-	public String getHistory () {
+
+	/**
+	 * This method will return string of all calculations done on the total
+	 * 
+	 * @return currently returns empty string TODO have return string of all
+	 *         calculations done
+	 */
+	public String getHistory() {
 		return "";
 	}
+	
 }
