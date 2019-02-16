23354c66218c9ffa2ae82ae8afb45fd3142a6443
Andrew Jones <andrew_52490@yahoo.com>
2019-02-15 19:00:14 -0700

    Added UpdateHistory method. All calculations recorded in history.

--- Assignment 2/src/cse360assign2/Calculator.java
+++ Assignment 2/src/cse360assign2/Calculator.java
@@ -5,8 +5,8 @@
  * 
  * This class acts as a calculator. It is able to do calculations such as
  * add, subtract, multiply and divide. All calculations are computed on the
- * running total which it initialized to zero.
- * 
+ * running total which it initialized to zero. History is tracked with values all 
+ * operations done on the running total. 
  * 
  */
 
@@ -15,9 +15,11 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String history;
 
 	public Calculator() {
-		total = 0; // not needed - included for clarity
+		total = 0; 
+		history = "0";
 	}
 
 	/**
@@ -30,37 +32,42 @@ public class Calculator {
 	}
 
 	/**
-	 * This method adds an integer value to the running total
+	 * This method adds an integer value to the running total 
+	 * Updates history with "+" and the value
 	 * 
 	 * @param value value which is added to the total
 	 */
 	public void add(int value) {
 		total += value;
+		updateHistory("+", value);
 	}
 
 	/**
-	 * This method subtracts an integer value from the running total
+	 * This method subtracts an integer value from the running total 
+	 * Updates history with "-" and the value
 	 * 
 	 * @param value value to be subtracted from the total
 	 */
 	public void subtract(int value) {
 		total -= value;
+		updateHistory("-", value);
 	}
 
 	/**
-	 *
-	 * This method multiplies an integer value by the running total
+	 * This method multiplies an integer value by the running total 
+	 * Updates history with "*" and the value
 	 * 
 	 * @param value value which the total is multiplied by
 	 */
 	public void multiply(int value) {
 		total *= value;
+		updateHistory("*", value);
 	}
 
 	/**
-	 * 
 	 * This method divides the running total by an integer value If the value passed
 	 * in is zero, the total is set to zero, else normal integer division is applied
+	 * Updates history with "/" and the value
 	 * 
 	 * @param value value which the total is divided by
 	 */
@@ -69,16 +76,27 @@ public class Calculator {
 			total = 0;
 		else
 			total /= value;
+		updateHistory("/", value);
+	}
+
+	/** 
+	 * This method concatenates the history string with all of the calculations that
+	 * are passed in
+	 * 
+	 * @param operation string to hold operation type, ie +,-,*,/
+	 * @param value     integer value that was computed with the total
+	 */
+	private void updateHistory(String operation, int value) {
+		history += " " + operation + " " + value;
 	}
 
 	/**
-	 * This method will return string of all calculations done on the total
+	 * This method returns a string of all calculations done on the total
 	 * 
-	 * @return currently returns empty string TODO have return string of all
-	 *         calculations done
+	 * @return hisory of all operations with concurrent values
 	 */
 	public String getHistory() {
-		return "";
+		return history;
 	}
-	
+
 }
